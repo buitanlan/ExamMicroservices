ff10f5577842beadd3e64e0430440c8bf9c77d4c
Lân Bùi Tấn <tanlanhcmus@gmail.com>
2022-04-27 01:03:14 +0700

    feat(): config identity server

--- Services/Identity/Identity.API/Program.cs
+++ Services/Identity/Identity.API/Program.cs
@@ -1,6 +1,87 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using System.Reflection;
+using Duende.IdentityServer.Services;
+using Identity.API.Database;
+using Identity.API.Models;
+using Identity.API.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder();
+var migrationsAssembly = typeof(Program).GetTypeInfo().Assembly.GetName().Name;
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddControllers();
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+       options.UseSqlServer(connectionString,
+       sqlServerOptionsAction: sqlOptions =>
+       {
+           sqlOptions.MigrationsAssembly(migrationsAssembly);
+           sqlOptions.EnableRetryOnFailure(
+               maxRetryCount: 5,
+               maxRetryDelay: TimeSpan.FromSeconds(30),
+               errorNumbersToAdd: null);
+       }));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddIdentityServer(x =>
+    {
+        x.IssuerUri = "https://tedu.com.vn";
+        x.Authentication.CookieLifetime = TimeSpan.FromHours(2);
+    })
+    .AddDeveloperSigningCredential()
+    .AddAspNetIdentity<ApplicationUser>()
+    .AddConfigurationStore(options =>
+    {
+        options.ConfigureDbContext = builder => builder.UseSqlServer(connectionString,
+            sqlServerOptionsAction: sqlOptions =>
+            {
+                sqlOptions.MigrationsAssembly(migrationsAssembly);
+                sqlOptions.EnableRetryOnFailure(
+                    maxRetryCount: 5,
+                    maxRetryDelay: TimeSpan.FromSeconds(30),
+                    errorNumbersToAdd: null);
+            });
+    })
+    .AddOperationalStore(options =>
+    {
+        options.ConfigureDbContext = builder => builder.UseSqlServer(connectionString,
+            sqlServerOptionsAction: sqlOptions =>
+            {
+                sqlOptions.MigrationsAssembly(migrationsAssembly);
+                sqlOptions.EnableRetryOnFailure(maxRetryCount: 5,
+                    maxRetryDelay: TimeSpan.FromSeconds(30),
+                    errorNumbersToAdd: null);
+            });
+    });
+builder.Services.AddTransient<IProfileService, ProfileService>();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Identity.API", Version = "v1" });
+});
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Identity.API v1"));
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+
+app.UseRouting();
+
+app.UseIdentityServer();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
