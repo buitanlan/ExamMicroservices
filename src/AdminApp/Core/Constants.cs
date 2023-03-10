a22de1ce400e820961bec8939413b787a2ffdca9
Lân Bùi Tấn <tanlanhcmus@gmail.com>
2023-03-10 18:22:19 +0700

    feat(): create new question

--- src/AdminApp/Core/Constants.cs
+++ src/AdminApp/Core/Constants.cs
@@ -10,5 +10,12 @@ public class UrlConstants
 {
     public const string Login = "/auth/login";
     public const string Dashboard = "/personal/dashboard";
-    public const string Categories = "/app/categories";
+    public const string Categories = "/app/categories";        
+    public const string Questions = "/app/questions";
+}
+
+public class DialogMode
+{
+    public const string Edit = "Edit";
+    public const string Add = "Add";
 }
