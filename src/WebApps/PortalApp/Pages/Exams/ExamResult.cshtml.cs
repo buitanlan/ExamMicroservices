a91e8d76c5db2c6c532a18e5608cf87ad534398b
BÙI TẤN LÂN <tanlanhcmus@gmail.com>
2023-11-16 16:35:39 +0700

    feat(): view exam result

--- src/WebApps/PortalApp/Pages/Exams/ExamResult.cshtml.cs
+++ src/WebApps/PortalApp/Pages/Exams/ExamResult.cshtml.cs
@@ -1,10 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Examination.Shared.ExamResults;
+using Examination.Shared.Exams;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PortalApp.Services.Interfaces;
 
 namespace PortalApp.Pages.Exams;
 
-public class ExamResultModel : PageModel
+public class ExamResultModel(IExamResultService examResultService, IExamService examService) : PageModel
 {
-    public void OnGet()
+    [BindProperty]
+    public ExamResultDto ExamResult { set; get; }
+
+    [BindProperty]
+    public ExamDto Exam { set; get; }
+
+    public async Task OnGetAsync(string examResultId)
     {
+        var result = await examResultService.GetExamResultByIdAsync(examResultId);
+        if (result.IsSucceed)
+        {
+            ExamResult = result.ResultObj;
+
+            var exam = await examService.GetExamByIdAsync(ExamResult.ExamId);
+            if (exam.IsSucceed) { Exam = exam.ResultObj; }
+        }
+    }
+
+    public string GetCharacterByIndex(int index)
+    {
+        const string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        var value = "";
+
+        if (index >= letters.Length)
+            value += letters[index / letters.Length - 1];
+
+        value += letters[index % letters.Length];
+
+        return value;
     }
 }
